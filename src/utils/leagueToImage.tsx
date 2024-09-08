8074c66a04fac0fabc5b2569879da5591f71f997
LIMIT <guzman.brandon12@Live.com>
2024-09-08 12:35:33 -0600

    fix: image

--- src/utils/leagueToImage.tsx
+++ src/utils/leagueToImage.tsx
@@ -1,12 +1,15 @@
+import Image from "next/image";
+
 export default (rank: string, height?: number) => {
 
   return (
-    <img
+    <Image
       className="inline"
       title={`${rank} league`}
       width={height ?? 60}
       alt={`${rank} league`}
       src={`/images/${rank.toLowerCase().replace(" ", "-")}.png`}
+      height={height ?? 60}
     />
   );
 };
