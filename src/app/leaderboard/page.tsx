39a365e466999298d153b2432068fa7ba8a377b5
LIMIT <guzman.brandon12@Live.com>
2024-09-09 00:10:17 -0600

    feat: center leaderboard and add disclaimer

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -2,6 +2,15 @@ import LeaderboardWrapper from "./wrapper";
 
 export default function Leaderboard() {
     return (
-        <LeaderboardWrapper/>
+        <div className="flex flex-col items-center">
+                <LeaderboardWrapper/>
+                <p className="w-full text-center text-xs leading-5 sm:text-sm">
+                    This site is <b>not</b> affiliated with <b>Mountaintop Studios</b> and all
+                    associated properties “Spectre Divide” are trademarks or registered
+                    trademarks of <b>Mountaintop Studios</b>.
+                </p>
+        </div>
+
+        
     )
 }
