79e9cd8644495747355e73e8bcad661b55bfab92
LIMIT <guzman.brandon12@Live.com>
2024-09-16 20:58:16 -0600

    fix:  add disclaimer about champion rankings

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -8,8 +8,8 @@ export default function Leaderboard() {
                     This site is <b>not</b> affiliated with <b>Mountaintop Studios</b> and all
                     associated properties “Spectre Divide” are trademarks or registered
                     trademarks of <b>Mountaintop Studios</b>.
-                    
                 </p>
+                <p><i>Champion rankings may not be accurate. We are currently gathering data. Update soon.</i></p>
                 <p className="w-full text-center text-xs leading-5 sm:text-sm">Message me on Discord at <b>limitedio</b> if you have any questions or concerns.</p>
         </div>
 
