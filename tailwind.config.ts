6f8249e8901faa15f26fcff3c53c8b0e72e4827d
Kadir Lofca <kadirlofca@outlook.com>
2024-09-26 02:46:13 -0400

    Added some style, added layout component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,17 @@ import type { Config } from 'tailwindcss'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: {
+					0: "hsl(var(--dark-0) / <alpha-value>)",
+					1: "hsl(var(--dark-1) / <alpha-value>)",
+					2: "hsl(var(--dark-2) / <alpha-value>)",
+				},
+
+				accent: "hsl(var(--accent) / <alpha-value>)"
+			}
+		},
 	},
 	plugins: [],
 } satisfies Config
