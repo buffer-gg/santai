4977db1d616f56a912fa4e9c22da7f4ef41e42e1
Kadir Lofca <kadirlofca@outlook.com>
2024-09-26 15:20:54 -0400

    Added placeholder searchbar visual

--- src/components/navigation/SearchBar.svelte
+++ src/components/navigation/SearchBar.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { cn } from "../../utils/cn";
+  export let className: string;
 </script>
 
-<div class="max-w-48 rounded-full bg-white">Search...</div>
+<div class={cn("w-full max-w-96 p-2 rounded-full bg-white", className)}>Search...</div>
