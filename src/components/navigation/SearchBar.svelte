fc3771855a8d93840fd3d7a3f2f2c896e09685e9
Kadir Lofca <kadirlofca@outlook.com>
2024-09-26 15:49:00 -0400

    Added gold search button but it looks like a cigarette now

--- src/components/navigation/SearchBar.svelte
+++ src/components/navigation/SearchBar.svelte
@@ -3,4 +3,7 @@
   export let className: string;
 </script>
 
-<div class={cn("w-full max-w-96 p-2 rounded-full bg-white", className)}>Search...</div>
+<div class={cn("flex flex-row w-full max-w-96 overflow-clip rounded-full bg-white", className)}>
+  <div class="p-2">Search...</div>
+  <div class="h-full w-24 ml-auto bg-accent"></div>
+</div>
