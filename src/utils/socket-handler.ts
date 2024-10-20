7d4f121f1dac713db8bf53abf0f4b22111a67979
LIMIT <guzman.brandon12@Live.com>
2024-10-20 10:11:20 -0600

    fix: update ws service url

--- src/utils/socket-handler.ts
+++ src/utils/socket-handler.ts
@@ -4,7 +4,7 @@ import { io, type Socket } from "socket.io-client";
 let socket: Socket | null = null;
 
 export function initializeSocket(playerId: string) {
-	socket = io("http://localhost:3004", {
+	socket = io("https://arcsentry-production.up.railway.app", {
 		query: { playerId },
 	});
 
